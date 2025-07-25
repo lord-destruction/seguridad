8efce99b412287887eed230003bc386034fb82db
wilmer fernandez <wilmer_ard@hotmail.com>
2025-07-25 10:43:42 -0500

    se modifico un script para genrar pass

--- cifrar.py
+++ cifrar.py
@@ -7,3 +7,14 @@ Created on Sat Jan 23 20:11:38 2021
 
 import random
 
+minuscula = "abcdefghijklmnñopqrstuvwxyz"
+mayuscula = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+numero = "0123456789"
+simbolo = "[]{}()+-*/;,:.-^`´~¿¡'?=&%$@!|¬°"
+
+all = minuscula+mayuscula+numero+simbolo
+length = 18
+
+pasword = "".join(random.sample(all,length))
+
+print(pasword)
